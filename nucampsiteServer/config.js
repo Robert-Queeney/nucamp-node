2ec86269058233e45eb4b0ca0fc0feb2ff64c19c
Robert Queeney <robert.queeney@gmail.com>
2021-01-30 16:49:02 -0600

    first commit

---
+++ nucampsiteServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite', 
+    'facebook': {
+        clientId: '769891680450102',
+        clientSecret: 'a7ecb7f0a10fec69d82c795cae160a5a'
+    }
+}
